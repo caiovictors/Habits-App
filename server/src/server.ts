bc4e2f92311353ae8386c9ad3527cd0bab285622
caiovictors <caiosarmento@eng.ci.ufpb.br>
2023-01-16 21:51:02 -0300

    first commit backend

---
+++ server/src/server.ts
@@ -0,0 +1,25 @@
+import Fastify from "fastify";
+import cors from '@fastify/cors'
+import { PrismaClient } from "@prisma/client";
+
+const app = Fastify()
+const prisma = new PrismaClient()
+
+app.register(cors)
+
+app.get('/', async () => {
+  const habits = await prisma.habit.findMany({
+    where: {
+      title: {
+        startsWith: 'hello'
+      }
+    }
+  })
+  return habits
+})
+
+app.listen({
+  port: 3333
+}).then(() => {
+  console.log('Server is running')
+})
