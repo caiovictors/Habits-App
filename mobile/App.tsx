d8349e1b6d40d775bd31dd6076f0be20ab42e75e
caiovictors <caiosarmento@eng.ci.ufpb.br>
2023-01-17 00:04:55 -0300

    first commit mobile

---
+++ mobile/App.tsx
@@ -0,0 +1,41 @@
+import { StyleSheet, Text, View, StatusBar } from 'react-native';
+import {
+  useFonts,
+  Inter_400Regular,
+  Inter_600SemiBold,
+  Inter_700Bold,
+  Inter_800ExtraBold
+} from '@expo-google-fonts/inter'
+import { Loading } from './src/components/Loading'
+
+export default function App() {
+
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_600SemiBold,
+    Inter_700Bold,
+    Inter_800ExtraBold
+  })
+
+  if (!fontsLoaded) return <Loading />
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Hahay</Text>
+      <StatusBar barStyle="light-content" backgroundColor='transparent' translucent />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#09090A',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    color: '#fff',
+    fontFamily: 'Inter_800ExtraBold',
+  }
+});
