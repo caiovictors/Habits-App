d5f5403d7022248723db9d31af344b34859305ce
caiovictors <caiosarmento@eng.ci.ufpb.br>
2023-01-18 00:17:25 -0300

    Adding components, views and utils to mobile

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View, StatusBar } from 'react-native';
+import { StatusBar } from 'react-native';
 import {
   useFonts,
   Inter_400Regular,
@@ -6,7 +6,9 @@ import {
   Inter_700Bold,
   Inter_800ExtraBold
 } from '@expo-google-fonts/inter'
+
 import { Loading } from './src/components/Loading'
+import { Home } from './src/screens/Home';
 
 export default function App() {
 
@@ -20,22 +22,9 @@ export default function App() {
   if (!fontsLoaded) return <Loading />
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Hahay</Text>
+    <>
+      <Home />
       <StatusBar barStyle="light-content" backgroundColor='transparent' translucent />
-    </View>
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#09090A',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  text: {
-    color: '#fff',
-    fontFamily: 'Inter_800ExtraBold',
-  }
-});
