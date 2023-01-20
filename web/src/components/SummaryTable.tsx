1e6b87d4ddfbc6ddc865052989419b1d91f48418
caiovictors <caiosarmento@eng.ci.ufpb.br>
2023-01-20 00:12:55 -0300

    Adding modal and popover to web

--- web/src/components/SummaryTable.tsx
+++ web/src/components/SummaryTable.tsx
@@ -22,7 +22,7 @@ export function SummaryTable() {
       </div>
       <div className="grid grid-rows-7 grid-flow-col gap-3">
         {summaryDates.map((date) => {
-          return <HabitDay key={date.toString()} />
+          return <HabitDay key={date.toString()} amount={5} completed={Math.round(Math.random() * 5)} />
         })}
         {amountOfDaysToFill > 0 && Array.from({ length: amountOfDaysToFill }).map((_, i) => {
           return <div key={i} className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg opacity-40 cursor-not-allowed"></div>
